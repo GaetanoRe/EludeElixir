283195f68db353e3b9984a23b2df74845ddf5b1d
Blunder <bbiker24@cox.net>
2024-07-24 06:54:59 -0700

    fixed audio
    Fixed audio by creating a dedicated autoload scene to allow playback during scene transitions.
    Added sfx to all buttons

---
+++ scripts/menus/start/SoundFX.gd
@@ -0,0 +1,7 @@
+extends Node
+
+func button_click():
+	$ButtonClick.play()
+
+func volume_click():
+	$VolumeClick.play()
--- scripts/menus/start/settings_menu.gd
+++ scripts/menus/start/settings_menu.gd
@@ -6,6 +6,7 @@ var tab_db: float
 var tab_linear: int
 var mute = -60
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	bus_Music = AudioServer.get_bus_index("Music")
@@ -16,38 +17,32 @@ func _ready():
 # Change Music volume when selected
 func _on_volume__music_tab_selected(tab):
 	if tab == 0:
+		SoundFx.volume_click()
 		AudioServer.set_bus_volume_db(bus_Music, mute)
-		print(tab)
-		print(AudioServer.get_bus_volume_db(bus_Music))
 
 	else: 
+		SoundFx.volume_click()
 		tab_db = -20 * ( log(11-tab) / log(10))
 		AudioServer.set_bus_volume_db(bus_Music,tab_db)
-		print(tab)
-		print(tab_db)
-		print(AudioServer.get_bus_volume_db(bus_Music))
-	
-	
-	
+
+
 
 # Change Sound Effects volume when selected
 func _on_volume__sound_effects_tab_selected(tab):
 	if tab == 0:
+		SoundFx.volume_click()
 		AudioServer.set_bus_volume_db(bus_SFX, mute)
-		print(tab)
-		print(AudioServer.get_bus_volume_db(bus_SFX))
 
 	else: 
+		SoundFx.volume_click()
 		tab_db = -20 * ( log(11-tab) / log(10))
 		AudioServer.set_bus_volume_db(bus_SFX,tab_db)
-		print(tab)
-		print(tab_db)
-		print(AudioServer.get_bus_volume_db(bus_SFX))
-	
+
 
 
 
 # Back Button - Change scene to Main Menu
 func _on_back_button_pressed():
+	SoundFx.button_click()
 	var next_scene = load("res://scenes/main_menu.tscn")
 	get_tree().change_scene_to_packed(next_scene)
