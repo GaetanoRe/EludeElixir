a03096897712a4a497ae863a7798bd635d147fb8
Gaetano Re <gaetanore@live.com>
2024-07-23 22:31:46 -0400

    added character controller and simple light detection

---
+++ scripts/player_movement/player.gd
@@ -0,0 +1,42 @@
+extends CharacterBody2D
+class_name Player
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var speed : float = 300.0
+var jumpVel : float = -500.0
+var playerVel : Vector2 = Vector2.ZERO
+var light_area : Area2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	light_area = get_node("LightDetection")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	
+	var direction = Input.get_vector("walk_left", "walk_right", "jump", "crouch")
+	if(direction.x != 0):
+		playerVel.x = direction.x * speed 
+	else:
+		playerVel.x = 0
+	
+	if(!is_on_floor()):
+		playerVel.y += gravity * delta
+	
+	if(Input.is_action_just_pressed("jump") and is_on_floor()):
+		playerVel.y += jumpVel
+	velocity = playerVel
+	move_and_slide()
+
+
+
+func _on_light_detection_area_entered(area):
+	if(area.is_in_group("lights")):
+		print("Character is touching light")
+
+
+func _on_light_detection_area_exited(area):
+	if(area.is_in_group("lights")):
+		print("Character has exited the light")
