5eec132b17754247ae8a1749a244398db0d86a28
Blunder <bbiker24@cox.net>
2024-07-30 21:04:33 -0700

    see merge comments

--- scripts/menus/start/SoundFX.gd
+++ scripts/menus/start/SoundFX.gd
@@ -6,14 +6,34 @@ func button_click():
 func volume_click():
 	$VolumeClick.play()
 
+
 func play_menu_music():
-	var song_progress = $MenuMusic.get_playback_position()
+	var menu_song_progress = $MenuMusic.get_playback_position()
 	$MenuMusic.play()
-	$MenuMusic.seek(song_progress)
+	$MenuMusic.seek(menu_song_progress)
 
-
-func fade_menu_music():
+func fade_out_menu_music():
 	var fade_music = get_tree().create_tween()
 	fade_music.tween_property($MenuMusic, "volume_db", -30, 1)
 	await get_tree().create_timer(1).timeout
 	$MenuMusic.stop()
+
+func fade_in_menu_music():
+	var fade_music = get_tree().create_tween()
+	fade_music.tween_property($MenuMusic, "volume_db", 0, 1)
+
+
+func play_dungeon_music():
+	var dung_song_progress = $DungeonMusic.get_playback_position()
+	$DungeonMusic.play()
+	$DungeonMusic.seek(dung_song_progress)
+
+func fade_in_dung_music():
+	var fade_dung_music = get_tree().create_tween()
+	fade_dung_music.tween_property($DungeonMusic, "volume_db", 0, 1)
+
+func fade_out_dung_music():
+	var fade_dung_music = get_tree().create_tween()
+	fade_dung_music.tween_property($DungeonMusic, "volume_db", -30, 1)
+	await get_tree().create_timer(1).timeout
+	$DungeonMusic.stop()
--- scripts/misc/alembic.gd
+++ scripts/misc/alembic.gd
@@ -2,16 +2,16 @@ extends Area2D
 class_name Alembic
 
 
+@onready var animation = $AnimatedSprite2D
+
 @export var alembic_id = 1
 @export var interact_type = "none"
 @export var interact_label = "none"
 
 
-
-
 # Called when the node enters the scene tree for the first time.
-#func _ready():
-	#pass # Replace with function body.
+func _ready():
+	animation.play("Alembic_Anim")
 #
 #
 ## Called every frame. 'delta' is the elapsed time since the previous frame.
